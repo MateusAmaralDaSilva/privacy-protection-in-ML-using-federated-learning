7ab60693e191a5f85717c120410825a03d6c8c5c
Mateus <mateus.amaral@guidance.dev>
2026-05-25 10:12:55 -0300

    dataset fix

--- dataset.py
+++ dataset.py
@@ -1,50 +1,58 @@
 ## implementar classe dataset:
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from flwr_datasets import FederatedDataset
+from sklearn.compose import ColumnTransformer
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OrdinalEncoder, StandardScaler
+from torch.utils.data import DataLoader, TensorDataset
+from flwr_datasets.partitioner import IidPartitioner
 
 class dataset():
     
     def load_data(partition_id: int, num_partitions):
-
-    global fds
-    if fds is None:
+        
         partitioner = IidPartitioner(num_partitions=num_partitions)
         fds = FederatedDataset(
             dataset="scikit-learn/adult-census-income",
             partitioners={"train": partitioner},
         )
-
-    dataset = fds.load_partition(partition_id, "train").with_format("pandas")[:]
-
-    dataset.dropna(inplace=True)
-
-    categorical_cols = dataset.select_dtypes(include=["object"]).columns
-    ordinal_encoder = OrdinalEncoder()
-    dataset[categorical_cols] = ordinal_encoder.fit_transform(dataset[categorical_cols])
-
-    X = dataset.drop("income", axis=1)
-    y = dataset["income"]
-
-    X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.2, random_state=42
-    )
-
-    numeric_features = X.select_dtypes(include=["float64", "int64"]).columns
-    numeric_transformer = Pipeline(steps=[("scaler", StandardScaler())])
-
-    preprocessor = ColumnTransformer(
-        transformers=[("num", numeric_transformer, numeric_features)]
-    )
-
-    X_train = preprocessor.fit_transform(X_train)
-    X_test = preprocessor.transform(X_test)
-
-    X_train_tensor = torch.tensor(X_train, dtype=torch.float32)
-    X_test_tensor = torch.tensor(X_test, dtype=torch.float32)
-    y_train_tensor = torch.tensor(y_train.values, dtype=torch.float32).view(-1, 1)
-    y_test_tensor = torch.tensor(y_test.values, dtype=torch.float32).view(-1, 1)
-
-    train_dataset = TensorDataset(X_train_tensor, y_train_tensor)
-    test_dataset = TensorDataset(X_test_tensor, y_test_tensor)
-    train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
-    test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False)
-
-    return train_loader, test_loader
+    
+        dataset = fds.load_partition(partition_id, "train").with_format("pandas")[:]
+    
+        dataset.dropna(inplace=True)
+    
+        categorical_cols = dataset.select_dtypes(include=["object"]).columns
+        ordinal_encoder = OrdinalEncoder()
+        dataset[categorical_cols] = ordinal_encoder.fit_transform(dataset[categorical_cols])
+    
+        X = dataset.drop("income", axis=1)
+        y = dataset["income"]
+    
+        X_train, X_test, y_train, y_test = train_test_split(
+            X, y, test_size=0.2, random_state=42
+        )
+    
+        numeric_features = X.select_dtypes(include=["float64", "int64"]).columns
+        numeric_transformer = Pipeline(steps=[("scaler", StandardScaler())])
+    
+        preprocessor = ColumnTransformer(
+            transformers=[("num", numeric_transformer, numeric_features)]
+        )
+    
+        X_train = preprocessor.fit_transform(X_train)
+        X_test = preprocessor.transform(X_test)
+    
+        X_train_tensor = torch.tensor(X_train, dtype=torch.float32)
+        X_test_tensor = torch.tensor(X_test, dtype=torch.float32)
+        y_train_tensor = torch.tensor(y_train.values, dtype=torch.float32).view(-1, 1)
+        y_test_tensor = torch.tensor(y_test.values, dtype=torch.float32).view(-1, 1)
+    
+        train_dataset = TensorDataset(X_train_tensor, y_train_tensor)
+        test_dataset = TensorDataset(X_test_tensor, y_test_tensor)
+        train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
+        test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False)
+    
+        return train_loader, test_loader
